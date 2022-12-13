ad25525a83e56064dedb26d9326a78e5ab12c320
tugstsog22 <btsogoo854@gmail.com>
2022-12-13 13:51:08 +0800

    New Random Sentences Generator Game

---
+++ RandomSentences.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+
+public class RandomSentences {
+    public static void main(String[] args) {
+
+        String[] names = {"Peter", "Michell", "Jane", "Steve"};
+        String[] places = {"Sofia", "Plovdiv", "Varna", "Burgas"};
+        String[] verbs = {"eats", "holds", "sees", "plays with", "brings"};
+        String[] nouns = {"stones", "cake", "apple", "laptop", "bikes"};
+        String[] adverbs = {"slowly", "diligently", "warmly", "sadly", "rapidly"};
+        String[] details = {"near the river", "at home", "in the park"};
+
+        System.out.println("Hello, this is your first random-generated sentence:");
+
+        while (true){
+            String randomName = getRandomWord(names);
+            String randomPlace = getRandomWord(places);
+            String randomVerb = getRandomWord(verbs);
+            String randomNoun = getRandomWord(nouns);
+            String randomAdverb = getRandomWord(adverbs);
+            String randomDetail = getRandomWord(details);
+
+            System.out.printf("%s from %s %s %s %s %s%n", randomName, randomPlace, randomVerb, randomNoun,randomAdverb,randomDetail);
+            System.out.println("Click [Enter] go generate a new one.");
+        }
+
+
+    }
+
+    public static String getRandomWord(String[] words) {
+        Random random = new Random();
+        int randomIndex = random.nextInt(words.length);
+        String word = words[randomIndex];
+        return word;
+    }
+}
